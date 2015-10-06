c76fc72530becfcd562aba5cf49f01c869f8ddce
Ben <ben.f.linn@gmail.com>
2015-10-06 12:25:32 -0500

    loaded http package, got api working on startup for sample location

--- server/startup.js
+++ server/startup.js
@@ -1,52 +1,33 @@
-var sampleAPI = {
-    "id": "4e6fe1404b90c00032eeac34",
-    "createdAt": 1315955008,
-    "type": "checkin",
-    "timeZoneOffset": -240,
-    "user": {
-        "id": "1",
-        "firstName": "Jimmy",
-        "lastName": "Foursquare",
-        "photo": "https://foursquare.com/img/blank_boy.png",
-        "gender": "male",
-        "homeCity": "New York, NY",
-        "relationship": "self"
-    },
-    "venue": {
-        "id": "4ab7e57cf964a5205f7b20e3",
-        "name": "foursquare HQ",
-        "contact": {
-            "twitter": "foursquare"
-        },
-        "location": {
-            "address": "East Village",
-            "lat": 40.72809214560253,
-            "lng": -73.99112284183502,
-            "city": "New York",
-            "state": "NY",
-            "postalCode": "10003",
-            "country": "USA"
-        },
-        "categories": [
-            {
-                "id": "4bf58dd8d48988d125941735",
-                "name": "Tech Startup",
-                "pluralName": "Tech Startups",
-                "shortName": "Tech Startup",
-                "icon": "https://foursquare.com/img/categories/building/default.png",
-                "parents": [
-                    "Professional & Other Places",
-                    "Offices"
-                ],
-                "primary": true
-            }
-        ],
-        "verified": true,
-        "stats": {
-            "checkinsCount": 7313,
-            "usersCount": 565,
-            "tipCount": 128
-        },
-        "url": "http://foursquare.com"
-    }
-}
+Meteor.startup(function() {
+
+HTTP.call( 'GET', 'https://api.foursquare.com/v2/venues/search', {
+  params: {
+    "client_id": "A5UA3LYLAL1V0EZ1EPAVSP5M2RV2GKWIE05VOIB2PSN2Z0KT",
+    "client_secret": "FTIBM0VRK3VTH22HZG5DUDTVZR13N3FI05Z1VFNN25PM3LXU",
+    "v": "20130815",
+    "ll": "40.7, -74",
+    "query": "bars"
+  }
+}, function( error, response ) {
+  if ( error ) {
+    console.log( error );
+  } else {
+    console.log( response );
+    /*
+     This will return the HTTP response object that looks something like this:
+     {
+       content: "String of content...",
+       data: [{
+         "body": "The body of the post with the ID 5."
+         "id": 5,
+         "title": "The title of the post with the ID 5.",
+         "userId": 1
+       }],
+       headers: {  Object containing HTTP response headers }
+       statusCode: 200
+     }
+    */
+  }
+});
+
+});
