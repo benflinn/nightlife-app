5fd6a28d16857d4197dfe7d489dbcda876a6c4a4
abraxasrex <jbreasor@gmail.com>
2015-10-06 13:29:58 -0500

    scaffolding for http data and db

---
+++ client/hit.js
@@ -0,0 +1,36 @@
+Template.hit.events({
+	'click .attend': function(){
+		var hitID= $(event.currentTarget).parent('.hit').data('id');
+		var count= $(event.currentTarget).parent('.hit').attendance;
+		var increase=count+1;
+		////somehow we'll get attendance as part of the hit.html dom or something///
+	    
+	    if(Meteor.userId()){
+		Locations.update(
+		  {_id: hitID},
+	      {$inc: {attendance: 1}}
+		  )
+		// eventually we will prevent attending more than once for the same user///
+	     }
+    }
+	
+});
+
+Template.hit.helpers({
+	image: function(){
+		return "prettypicture.jpg";
+	},
+	name: function(){
+		return "coolname barplace";
+	},
+	review: function(){
+		return "this place is sweet";
+	},
+	link: function(){
+		return "www.coolbar.com";
+	},
+	attendance: function(){
+		return "this object's attendance";
+	}
+	
+});
---
+++ collections/locations.js
@@ -0,0 +1,3 @@
+Locations =new Mongo.Collection('locations');
+
+//idea: add an allow() for insert() that restricts our results to only the top relevant 10-20 results//
