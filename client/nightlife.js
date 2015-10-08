5d2eb281369f5ec0bebceb9530f3f4d7b7a0d651
Ben <ben.f.linn@gmail.com>
2015-10-08 15:44:31 -0500

    GOT THE PICTURES TO WORK

--- client/nightlife.js
+++ client/nightlife.js
@@ -37,17 +37,31 @@
 		var ID= place.id;
 		var url= place.url;
 
-		var eachplace={
-			venueId: ID,
-			name: placename,
-			link: url
-		};
-		  MyHits.insert(eachplace);
+    HTTP.call( 'GET', 'https://api.foursquare.com/v2/venues/'+ID+'/photos', {
+      params: {
+        "client_id": "A5UA3LYLAL1V0EZ1EPAVSP5M2RV2GKWIE05VOIB2PSN2Z0KT",
+        "client_secret": "FTIBM0VRK3VTH22HZG5DUDTVZR13N3FI05Z1VFNN25PM3LXU",
+        "v": "20130815",
+        }
+      }, function( error, response ) {
+        if ( error ) {
+          console.log( error );
+          } else {
+            var prefix = response.data.response.photos.items[0].prefix;
+            var suffix = response.data.response.photos.items[0].suffix;
+
+            var eachplace={
+              venueId: ID,
+              name: placename,
+              link: url,
+              photolink: prefix + "300x300" + suffix
+              };
+              MyHits.insert(eachplace);
+              }
+              });
     });
 	 
-	
-	
-	
+
 	
 	
 	//////////////////////////////////////////////////////////////////////////////////////
